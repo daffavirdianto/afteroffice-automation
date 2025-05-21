e9dd2863f0b4b1ba64d83e2d680197973fe3f9fd
daffavirdianto <daffa.virdianto@gmail.com>
2025-05-21 16:42:13 +0700

    [Task 2] Implementasi E2E Test With POJO + Lombok

---
+++ src/main/java/com/apiautomation/model/RequestLogin.java
@@ -0,0 +1,15 @@
+package com.apiautomation.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class RequestLogin {
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+}
---
+++ src/main/java/com/apiautomation/model/ResponseUpdateObject.java
@@ -0,0 +1,40 @@
+package com.apiautomation.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class ResponseUpdateObject {
+@JsonProperty("id")
+    private int id;
+
+    @JsonProperty("name")
+    private String name;
+    
+    private DataObject data;
+
+    @Data
+    public static class DataObject {
+        @JsonProperty("year")
+        private int year;
+
+        @JsonProperty("price")
+        private double price;
+
+        @JsonProperty("CPU model")
+        private String cpu_model;
+
+        @JsonProperty("Hard disk size")
+        private String hard_disk_size;
+
+        @JsonProperty("capacity")
+        private String capacity;
+
+        @JsonProperty("screen_size")
+        private String screen_size;
+
+        @JsonProperty("color")
+        private String color;
+    }
+}
