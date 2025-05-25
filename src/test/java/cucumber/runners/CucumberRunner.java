734b801ca0ea4961bb79d53a689397ea65d1de02
daffavirdianto <daffa.virdianto@gmail.com>
2025-05-25 21:03:10 +0700

    update cucumber report

--- src/test/java/cucumber/runners/CucumberRunner.java
+++ src/test/java/cucumber/runners/CucumberRunner.java
@@ -2,6 +2,7 @@ package cucumber.runners;
 
 import org.testng.annotations.AfterSuite;
 
+import cucumber.helper.GenerateReport;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -15,6 +16,7 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
 
     @AfterSuite
     public void after_suite() {
+        GenerateReport.generateReport();
         System.out.println("Cucumber runner start...");
     }
 }
